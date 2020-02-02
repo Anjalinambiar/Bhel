f5e1d8746e6b9917a75f7a060c9c0f23ba97fd02
Anjalinambiar <anjali.muraleedharanpk@gmail.com>
2020-02-02 17:53:08 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[ ] args) {
+        System.out.println("Hello Java");
+         Bhelcontainer bhelcontainer = new Bhelcontainer();
+         bhelcontainer.Addqty("puffedrice",10);
+    }
+}
