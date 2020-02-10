1fa6c8de5a53e8e310970032660d8ecdc05c0a42
Anjalinambiar <anjali.muraleedharanpk@gmail.com>
2020-02-10 23:07:20 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
-class Main {
-    public static void main(String[ ] args) {
-        System.out.println("Hello Java");
+public class Bhel
+{
+  
+  public static void main(String[] args)
+  {
+   System.out.println("Hello Java");
          Bhelcontainer bhelcontainer = new Bhelcontainer();
          bhelcontainer.Addqty("puffedrice",10);
-    }
+    	 bhelcontainer.Addqty("salt",2);
+    	  bhelcontainer.Addqty("groundnuts",10);
+  }
 }
